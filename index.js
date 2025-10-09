fed20aaebc53c9773ee34edb9c6019f2374a3b4c
Kristine Anselle Calaluan <21-02623@g.batstate-u.edu.ph>
2025-10-09 10:00:57 +0800

    Added code

--- index.js
+++ index.js
@@ -3,24 +3,22 @@
     // JavaScript Classes
     JavaScript Classes are templates for JavaScript Objects.
 
-
-
 */
 
-class Person {
-    constructor(name, year, address) {
-        this.name = name;
-        this.year = year;
-        this.address = address;
-    }
-    age() {
-        let date = new Date();
-        return date.getFullYear() - this.year
-    }
-    intro() {
-        return `Hi I am ${this.name}, born on year ${this.year}. I live in ${this.address}`;
-    }
-}
+    // class Person {
+    //     constructor(name, year, address) {
+    //         this.name = name;
+    //         this.year = year;
+    //         this.address = address;
+    //     }
+    //     age() {
+    //         let date = new Date();
+    //         return date.getFullYear() - this.year
+    //     }
+    //     intro() {
+    //         return `Hi I am ${this.name}, born on year ${this.year}. I live in ${this.address}`;
+    //     }
+    // }
 
 
 /*
@@ -110,94 +108,94 @@ const person3 = {};
 
 
 
-class Animal {
-    constructor (name, type, diet) {
-        this.name = name;
-        this.type = type;
-        this.diet = diet;
-    }
-    speak(sound) {
-        return `${sound}!`
-    }
-    get getName() {
-        return this.name;
-    }
-    set setName(nameParam) {
-        this.name = nameParam
-    }
-}
+// class Animal {
+//     constructor (name, type, diet) {
+//         this.name = name;
+//         this.type = type;
+//         this.diet = diet;
+//     }
+//     speak(sound) {
+//         return `${sound}!`
+//     }
+//     get getName() {
+//         return this.name;
+//     }
+//     set setName(nameParam) {
+//         this.name = nameParam
+//     }
+// }
 
-const dog = new Animal('Shih Tzu', 'Mammal', 'Carnivore');
-console.log(dog);
-console.log(dog.speak('Arf'));
+// const dog = new Animal('Shih Tzu', 'Mammal', 'Carnivore');
+// console.log(dog);
+// console.log(dog.speak('Arf'));
 
-console.log('----------------');
-console.log(dog.getName);
-dog.setName = 'Bulldog';
-console.log(dog.getName);
-console.log('----------------');
+// console.log('----------------');
+// console.log(dog.getName);
+// dog.setName = 'Bulldog';
+// console.log(dog.getName);
+// console.log('----------------');
 
-class Bird extends Animal {
-    constructor (name, type, diet, color) {
-        super(name, type, diet);
-        this.color = color;
-    }
-    show () {
-        return `I am color ${this.color}`;
-    }
-}
+// class Bird extends Animal {
+//     constructor (name, type, diet, color) {
+//         super(name, type, diet);
+//         this.color = color;
+//     }
+//     show () {
+//         return `I am color ${this.color}`;
+//     }
+// }
 
-const bird = new Bird('Bald Eagle', 'Aerial', 'Carnivore', 'Golden');
-console.log(bird);
-console.log(bird.speak('Klee-Klee'));
+// const bird = new Bird('Bald Eagle', 'Aerial', 'Carnivore', 'Golden');
+// console.log(bird);
+// console.log(bird.speak('Klee-Klee'));
 
-class Duck extends Bird {
-    constructor (name, type, diet, color, habitat) {
-        super(name, type, diet, color);
-        this.habitat = habitat;
-    }
-    intro () {
-        return `Hi, I am ${this.name}, I live in a ${this.habitat}`;
-    }
-}
+// class Duck extends Bird {
+//     constructor (name, type, diet, color, habitat) {
+//         super(name, type, diet, color);
+//         this.habitat = habitat;
+//     }
+//     intro () {
+//         return `Hi, I am ${this.name}, I live in a ${this.habitat}`;
+//     }
+// }
 
-const donald = new Duck('Donald', 'Mallard', 'Fish', 'Blue-Green', 'Pond');
-console.log(donald);
-console.log(donald.intro());
-console.log(donald.show());
-console.log(donald.speak('Quack'));
+// const donald = new Duck('Donald', 'Mallard', 'Fish', 'Blue-Green', 'Pond');
+// console.log(donald);
+// console.log(donald.intro());
+// console.log(donald.show());
+// console.log(donald.speak('Quack'));
 
-introduction('KIRK');
+// introduction('KIRK');
 
-// Dine-fine si function
-function introduction (name) {
-    console.log(`Hi am ${name}`);
-}
+// // Dine-fine si function
+// function introduction (name) {
+//     console.log(`Hi am ${name}`);
+// }
 
 
 
-class Car {
-    constructor(name) {
-      this.name = name;
-    }
-    static hello() {
-      return "Hello!!";
-    }
-  }
+// class Car {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//     static hello() {
+//       return "Hello!!";
+//     }
+//   }
   
-  const myCar = new Car("Ford");
-  console.log(Car.hello());
+//   const myCar = new Car("Ford");
+//   console.log(Car.hello());
 
 
-  class Model extends Car {
-    constructor (name, model) {
-        super(name);
-        this.model = model;
-    }
-  }
+//   class Model extends Car {
+//     constructor (name, model) {
+//         super(name);
+//         this.model = model;
+//     }
+//   }
 
-  const toyota = new Model('Car Name', 'Toyota');
-  console.log(Model.hello());
+//   const toyota = new Model('Car Name', 'Toyota');
+//   console.log(Model.hello());
 
 /* --------------------------------------------- 1 --------------------------------------------- */
   
@@ -207,6 +205,26 @@ class Car {
     Add a method introduce() that returns "Hi, I'm [name] and I'm [age] years old"
     Create 3 instances and call their methods
 */
+
+    // class Person {
+    //     constructor(name, age, email) {
+    //         this.name = name;
+    //         this.age = age;
+    //         this.email = email;
+    // }
+
+    // introduce() {
+    //     return `Hi, I'm ${this.name} and I'm ${this.age} years old.`;
+    //     }
+    // }
+
+    // const person1 = new Person("Kristine", 22, "kristine@example.com");
+    // const person2 = new Person("Anselle", 22, "anselle@example.com");
+    // const person3 = new Person("Kirk", 25, "kirk@example.com");
+
+    // console.log(person1.introduce());
+    // console.log(person2.introduce());
+    // console.log(person3.introduce());
 
 /* --------------------------------------------- 1 --------------------------------------------- */
 
@@ -220,6 +238,42 @@ class Car {
     Make sure withdraw doesn't allow negative balance
     Test with multiple accounts
 */
+
+    // class BankAccount {
+    // constructor(accountNumber, balance) {
+    //     this.accountNumber = accountNumber;
+    //     this.balance = balance;
+    // }
+
+    // deposit(amount) {
+    //     this.balance += amount;
+    //     console.log(`Deposited ₱${amount}. New balance: ₱${this.balance}`);
+    // }
+
+    // withdraw(amount) {
+    //     if (amount > this.balance) {
+    //     console.log(`Withdrawal failed. Insufficient balance in account ${this.accountNumber}.`);
+    //     } else {
+    //     this.balance -= amount;
+    //     console.log(`Withdrew ₱${amount}. Remaining balance: ₱${this.balance}`);
+    //     }
+    // }
+
+    // getBalance() {
+    //     return `Account ${this.accountNumber} balance: ₱${this.balance}`;
+    // }
+    // }
+
+    // const acc1 = new BankAccount(1001, 5000);
+    // const acc2 = new BankAccount(1002, 10000);
+
+    // acc1.deposit(1500);
+    // acc1.withdraw(2000);
+    // console.log(acc1.getBalance());
+
+    // acc2.withdraw(12000);
+    // acc2.deposit(3000);
+    // console.log(acc2.getBalance());
 
 /* --------------------------------------------- 2 --------------------------------------------- */
 
@@ -236,7 +290,27 @@ class Car {
 
 /* --------------------------------------------- 3 --------------------------------------------- */
 
+    class Temperature {
+        constructor(celsius) {
+            this.celsius = celsius;
+        }
+    get fahrenheit() {
+        return (this.celsius * 9 / 5) + 32;
+        }
+    set fahrenheit(value) {
+        this.celsius = (value - 32) * 5 / 9;
+        }
 
+    display() {
+        console.log(`Temperature: ${this.celsius.toFixed(2)}°C / ${this.fahrenheit.toFixed(2)}°F`);
+        }
+    }
+
+    const temp = new Temperature(25);
+    temp.display();
+
+    temp.fahrenheit = 100;
+    temp.display(); 
 
 /* --------------------------------------------- 4 --------------------------------------------- */
 
@@ -246,6 +320,35 @@ class Car {
     Add a static method circleArea(radius) that uses the static PI property
     Call these without creating an instance
 */
+
+    class Calculator {
+        static PI = 3.14159;
+        static add(a, b) {
+            return a + b;
+         }
+        static subtract(a, b) {
+            return a - b;
+        }
+        static multiply(a, b) {
+            return a * b;
+        }
+        static divide(a, b) {
+            if (b == 0) {
+            return "Error: Cannot divide by zero!";
+            }
+            return a / b;
+        }
+        static circleArea(radius) {
+            return this.PI * radius * radius;
+        }
+    }
+
+    console.log("Add:", Calculator.add(10, 5));
+    console.log("Subtract:", Calculator.subtract(10, 5));
+    console.log("Multiply:", Calculator.multiply(10, 5));
+    console.log("Divide:", Calculator.divide(10, 5));
+    console.log("PI:", Calculator.PI);
+    console.log("Area of circle (r=3):", Calculator.circleArea(3));
 
 /* --------------------------------------------- 4 --------------------------------------------- */
 
@@ -262,7 +365,38 @@ class Car {
 
 /* --------------------------------------------- 5 --------------------------------------------- */
 
+    class User {
+    #password;
 
+    constructor(username) {
+        this.username = username;
+    }
+
+    setPassword(newPassword) {
+        if (newPassword.length < 8) {
+        console.log("Password must be at least 8 characters long.");
+        } else {
+            this.#password = newPassword;
+            console.log("Password set successfully.");
+        }
+    }
+
+    validatePassword(input) {
+        if (input == this.#password) {
+            console.log("Access granted.");
+            return true;
+        } else {
+            console.log("Invalid password.");
+            return false;
+        }
+    }
+    }
+
+    const user1 = new User("anselle");
+    user1.setPassword("12345");
+    user1.setPassword("mySecret123");
+    user1.validatePassword("wrong");
+    user1.validatePassword("mySecret123");
 
 /* --------------------------------------------- 6 --------------------------------------------- */
 
